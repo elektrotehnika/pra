5706c6abc1dd2f99ac9c82c033c3357639a9f55a
djoto <djordjegacic99tb@gmail.com>
2025-12-28 16:53:33 +0100

    Додата демонстрација специјалних дозвола у уводним белешкама за Linux.

---
+++ материјали/write_to_file.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    // Definisanje promenljive za naziv datoteke
+    char filename[100];
+
+    // Traži od korisnika da unese naziv datoteke
+    printf("Unesite naziv datoteke: ");
+    fgets(filename, sizeof(filename), stdin);  // Unos sa tastature
+
+    // Uklanjanje novog reda na kraju unosa (ako postoji)
+    filename[strcspn(filename, "\n")] = 0;
+
+    // Otvoriti datoteku u režimu samo za čitanje da proverimo da li postoji
+    FILE *file = fopen(filename, "r");  // Otvori datoteku samo za čitanje
+
+    if (file == NULL) {
+        // Ako datoteka ne postoji, ispiši grešku i izađi iz programa
+        perror("Datoteka ne postoji");
+        return 1;
+    }
+
+    fclose(file);  // Zatvaramo datoteku jer smo samo proverili njeno postojanje
+
+    // Otvoriti datoteku za upis
+    file = fopen(filename, "w");
+
+    if (file == NULL) {
+        perror("Nije moguće otvoriti datoteku za upis");
+        return 1;
+    }
+
+    // Upisivanje sadržaja u datoteku
+    fprintf(file, "Strogo definisan sadržaj datoteke %s.\n", filename);
+
+    // Zatvaranje fajla
+    fclose(file);
+
+    // Informacija o uspešnom upisu
+    printf("Sadržaj je uspešno upisan u datoteku %s.\n", filename);
+
+    return 0;
+}
